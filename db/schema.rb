e804692d05d129f6edb4b5d9ddb33ff207824589
Jacob Robertson <g.jacob.robertson@gmail.com>
2012-01-23 17:45:49 -0500

    added payment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111230201646) do
+ActiveRecord::Schema.define(:version => 20120123223930) do
 
   create_table "costs", :force => true do |t|
     t.integer  "user_id"
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(:version => 20111230201646) do
   add_index "groups_users", ["group_id"], :name => "index_groups_users_on_group_id"
   add_index "groups_users", ["user_id", "group_id"], :name => "index_groups_users_on_user_id_and_group_id", :unique => true
   add_index "groups_users", ["user_id"], :name => "index_groups_users_on_user_id"
+
+  create_table "payments", :force => true do |t|
+    t.string   "description"
+    t.decimal  "amount"
+    t.integer  "from_id"
+    t.integer  "to_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
