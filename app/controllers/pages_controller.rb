f269465da1a6ddcd63b9ea7cab6a9376228848c2
Jacob Robertson <jrobertson@quantisense.com>
2012-10-18 19:21:00 -0400

    Redesigned Landing Page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   before_filter :store_location
   def home
     @user = current_user
+    if @user.nil?
+      render 'landing', :layout => false
+    else
+      render 'home'
+    end
   end
 end
