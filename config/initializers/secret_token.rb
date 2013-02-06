0c60fb0956c7e801305b1e7cbf58b49073031c94
Jacob Robertson <g.jacob.robertson@gmail.com>
2013-02-06 15:28:58 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrapv2::Application.config.secret_token = 'c650d2e66f3158e5aed2930ba91ea1aa9b82178c28bb3524568fbbee3e7bebf6601eadc5f81235b653258f9afd42249bfeb357a4f72a35b786c225937ec0b0b0'
