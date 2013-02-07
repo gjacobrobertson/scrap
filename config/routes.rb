740377c6995214d8998438535c3b433498b9b7c2
Jacob Robertson <g.jacob.robertson@gmail.com>
2013-02-07 11:45:19 -0500

    Sign up, Sign in, Sign out

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Scrapv2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
