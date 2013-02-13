c7d5cc1507ec4a7e9fba3df39587c2c5ea043893
Jacob Robertson <g.jacob.robertson@gmail.com>
2013-02-12 19:30:55 -0500

    Facebook OAuth only

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,19 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  devise :rememberable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :remember_me, :provider, :uid, :name
   # attr_accessible :title, :body
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                           provider:auth.provider,
+                           uid:auth.uid,
+                           )
+    end
+    user
+  end
 end
