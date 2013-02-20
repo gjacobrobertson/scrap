2b6e788ff2815dde9baf07d5a5ec98c089a02fdf
Jacob Robertson <g.jacob.robertson@gmail.com>
2013-02-19 20:37:36 -0500

    Failing tests

--- test/unit/transaction_test.rb
+++ test/unit/transaction_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "fields" do
+    transaction = transactions(:one)
+
+    assert transaction.respond_to? :from_id
+    assert transaction.respond_to? :to_id
+
+    assert transaction.respond_to? :amount
+    assert transaction.respond_to? :note
+    assert trasnaction.respond_to? :label
+
+    assert transaction.respond_to? :confirmed
+  end
+
+  test "associations" do
+    transaction = transactions(:one)
+
+    assert transaction.respond_to? :from
+    assert transaction.respond_to? :to
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "fields" do
+    user = users(:one)
+    assert user.respond_to? :name
+    assert user.respond_to? :provider
+    assert user.respond_to? :uid
+  end
+
+  test "associations" do
+    user = users(:one)
+    assert user.respond_to? :debits
+    assert user.respond_to? :credits
+
+    assert user.debits.is_a? Array
+    assert user.credits.is_a? Array
+
+    puts user.debits
+    assert user.debits.size == 1
+  end
 end
