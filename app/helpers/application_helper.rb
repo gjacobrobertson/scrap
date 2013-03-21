48ff45aa4a0e711e90e1251959e31547e9b875ed
Jacob Robertson <g.jacob.robertson@gmail.com>
2013-03-21 16:37:30 -0400

    Better notification descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,22 +14,4 @@ module ApplicationHelper
   def facebook_profile_pic(user)
     image_tag("https://graph.facebook.com/#{user.uid}/picture")
   end
-
-  def approval_description(approval)
-    case approval.type
-    when "SplitTransaction"
-      "#{approval.from.name} split #{number_to_currency(approval.split_total)} #{"for #{approval.note}" if approval.note} with you #{"and #{approval.others_count} others" if approval.others_count > 0}"
-    else
-      "You owe #{approval.from.name} #{number_to_currency(approval.amount)} for #{approval.note}"
-    end
-  end
-
-  def rejection_description(rejection)
-    case rejection.type
-    when "SplitTransaction"
-      "#{rejection.to.name} contested your split of #{number_to_currency(rejection.split_total)} #{"for #{rejection.note}" if rejection.note}"
-    else
-      "TODO"
-    end
-  end
 end
--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,2 +1,25 @@
 module TransactionsHelper
+  def approval_description(approval)
+    case approval.type
+    when "SplitTransaction"
+      description = approval.note ? approval.note : 'a cost'
+      with = "you"
+      with = with + " and #{approval.others_count} others" if approval.others_count > 0
+      share = number_to_currency(approval.amount)
+      "#{approval.from.name} split #{description} with #{with}. Your share is #{share}"
+    else
+      "TODO"
+    end
+  end
+
+  def rejection_description(rejection)
+    case rejection.type
+    when "SplitTransaction"
+      description = rejection.note ? rejection.note : 'a cost you split'
+      share = number_to_currency(rejection.amount)
+      "#{rejection.to.name} rejected their share of #{share} for #{description}"
+    else
+      "TODO"
+    end
+  end
 end
