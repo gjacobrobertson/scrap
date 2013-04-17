0e939408e5e0ecd91a5377d566a5353243792666
Jacob Robertson <g.jacob.robertson@gmail.com>
2013-04-17 16:34:42 -0400

    specs for debt calcs with pending_or_approved scope

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,8 +16,8 @@ describe User do
   it { should respond_to :credit_subtotals }
   it { should respond_to :debt_subtotals }
   it { should respond_to :debt_to }
-  it { should respond_to :pending_approvals }
-  it { should respond_to :rejections }
+  it { should respond_to :notifications }
+  it { should respond_to :has_notifications }
   it { should respond_to :pending_for_user }
   it { should respond_to :history_for_user }
   it { should have_many :debits }
@@ -33,31 +33,40 @@ describe User do
     before do
       @debtor = FactoryGirl.create(:user)
       @creditor = FactoryGirl.create(:user)
-      @debt = FactoryGirl.create(:split, :from => @creditor, :with => [@user], :amount => 5)
-      @debt.split_transactions.each {|t| t.approve}
-      @credit = FactoryGirl.create(:split, :from => @user, :with => [@debtor], :amount => 10)
-      @credit.split_transactions.each{|t| t.approve}
-      @unconfirmed = FactoryGirl.create(:split, :from => @creditor, :with => [@user], :amount => 15)
-      @rejected = FactoryGirl.create(:split, :from => @user, :with => [@debtor], :amount => 20)
-      @rejected.split_transactions.each {|t| t.reject}
+
+      @approved_debt = FactoryGirl.create(:split, :from => @creditor, :with => [@user], :amount => 5)
+      @approved_debt.split_transactions.each {|t| t.approve}
+
+      @approved_credit = FactoryGirl.create(:split, :from => @user, :with => [@debtor], :amount => 10)
+      @approved_credit.split_transactions.each{|t| t.approve}
+
+      @pending_debt = FactoryGirl.create(:split, :from => @creditor, :with => [@user], :amount => 15)
+
+      @pending_credit = FactoryGirl.create(:split, :from => @user, :with => [@debtor], :amount => 20)
+
+      @rejected_debt = FactoryGirl.create(:split, :from => @creditor, :with => [@user], :amount => 25)
+      @rejected_debt.split_transactions.each {|t| t.reject}
+
+      @rejected_credit = FactoryGirl.create(:split, :from => @user, :with => [@debtor], :amount => 30)
+      @rejected_credit.split_transactions.each {|t| t.reject}
     end
 
     describe "total_debt" do
       subject { @user.total_debt }
       it { should be_a_kind_of Numeric }
-      it { should be == 2.50 }
+      it { should be == 10.00 }
     end
 
     describe "total_credit" do
       subject { @user.total_credit }
       it { should be_a_kind_of Numeric }
-      it { should be == 5 }
+      it { should be == 15.00 }
     end
 
     describe "total_net" do
       subject { @user.total_net }
       it { should be_a_kind_of Numeric }
-      it { should be == 2.50 }
+      it { should be == 5.00 }
     end
 
     describe "creditors" do
@@ -79,7 +88,7 @@ describe User do
       it { should be_a_kind_of Array }
       it { should have(1).items }
       it do
-        entry = { :user => @creditor, :amount => 2.50 }
+        entry = { :user => @creditor, :amount => 10.00 }
         should include entry
       end
     end
@@ -89,7 +98,7 @@ describe User do
       it { should be_a_kind_of Array }
       it { should have(1).items }
       it do
-        entry = { :user => @debtor, :amount => 5 }
+        entry = { :user => @debtor, :amount => 15.00 }
         should include entry
       end
     end
@@ -97,26 +106,20 @@ describe User do
     describe "debt_to" do
       describe "creditor" do
         subject { @user.debt_to @creditor }
-        it { should be == 2.5 }
+        it { should be == 10.00 }
       end
       describe "debtor" do
         subject { @user.debt_to @debtor }
-        it { should be == -5 }
+        it { should be == -15.00 }
       end
     end
 
-    describe "pending_approvals" do
-      subject { @user.pending_approvals }
+    describe "notifications" do
+      subject { @user.notifications }
       it { should be_a_kind_of Array }
-      it { should have(1).items }
-      it { should include @unconfirmed.split_transactions.first }
-    end
-
-    describe "rejections" do
-      subject { @user.rejections }
-      it { should be_a_kind_of Array }
-      it { should have(1).items }
-      it { should include @rejected.split_transactions.first }
+      it { should have(2).items }
+      it { should include @pending_debt.split_transactions.first }
+      it { should include @rejected_credit.split_transactions.first }
     end
   end
 
