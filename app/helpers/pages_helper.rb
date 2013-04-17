a905d725a9a8fb00eaa7221373f8a30609a97e49
Jacob Robertson <g.jacob.robertson@gmail.com>
2013-04-17 14:52:13 -0400

    total on user page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -9,4 +9,16 @@ module PagesHelper
       number_to_currency(amount)
     end
   end
+
+  def debt_total(user)
+    amount = current_user.debt_to(user)
+    case
+    when amount < 0
+      "Owes you #{number_to_currency(-1*amount)}"
+    when amount == 0
+      "All settled up"
+    when amount > 0
+      "You owe #{number_to_currency amount}"
+    end
+  end
 end
